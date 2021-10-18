a6db677d83e288c5bb66823486460f6a235dbc73
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2021-10-18 21:49:04 +0200

    Implemented SquareRoot  - approximates SquareRoots  - added Readme entry

---
+++ src/de/termitehuegel/basicAlgorithmsJava/math/basic/SquareRoot.java
@@ -0,0 +1,27 @@
+package de.termitehuegel.basicAlgorithmsJava.math.basic;
+
+/**
+ * @author termitehuegel
+ */
+public class SquareRoot {
+
+    public double squareRoot(double a, double currentGuess) {
+        //sqrt(0) = 0
+        if (a == 0) {
+            return 0;
+        }
+        //newton approximation for 0=x^2-a => is true when x=sqrt(a)
+        double newGuess = currentGuess - (currentGuess*currentGuess-a) / (2*currentGuess);
+        //if there has been large enough change => make another approximation
+        if (currentGuess - newGuess >= Double.MIN_VALUE) {
+            return squareRoot(a, newGuess);
+        }
+        //approximation is close enough
+        return newGuess;
+    }
+
+    public double squareRoot(double a) {
+        return squareRoot(a, a);
+    }
+
+}
