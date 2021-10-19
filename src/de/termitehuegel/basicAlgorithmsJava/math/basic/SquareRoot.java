1eca1944cf59ec7a0430c13130b7cdec124d8910
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2021-10-19 14:01:42 +0200

    Added support for all positiv real numbers

--- src/de/termitehuegel/basicAlgorithmsJava/math/basic/SquareRoot.java
+++ src/de/termitehuegel/basicAlgorithmsJava/math/basic/SquareRoot.java
@@ -6,6 +6,9 @@ package de.termitehuegel.basicAlgorithmsJava.math.basic;
 public class SquareRoot {
 
     public double squareRoot(double a, double currentGuess) {
+        if (a < 0) {
+            return Double.NaN;
+        }
         //sqrt(0) = 0
         if (a == 0) {
             return 0;
@@ -13,9 +16,13 @@ public class SquareRoot {
         //newton approximation for 0=x^2-a => is true when x=sqrt(a)
         double newGuess = currentGuess - (currentGuess*currentGuess-a) / (2*currentGuess);
         //if there has been large enough change => make another approximation
-        if (currentGuess - newGuess >= Double.MIN_VALUE) {
+        if (currentGuess - newGuess >= Double.MIN_VALUE && a >1) {
             return squareRoot(a, newGuess);
         }
+        if (newGuess - currentGuess >= Double.MIN_VALUE && a < 1) {
+            return squareRoot(a, newGuess);
+        }
+
         //approximation is close enough
         return newGuess;
     }
