e1a4e50b86c4180a76b4d4db385791ce874a565a
Philip Berner <philipberner.berlin@gmail.com>
2022-09-28 13:32:14 +0200

    added squareMultiply

--- src/de/termitehuegel/basicAlgorithmsJava/math/basic/Power.java
+++ src/de/termitehuegel/basicAlgorithmsJava/math/basic/Power.java
@@ -5,22 +5,57 @@ package de.termitehuegel.basicAlgorithmsJava.math.basic;
  */
 public class Power {
 
-    public double power(final double basis, final int exponent) {
+    public double power(final double base, final int exponent) {
         //0^0 and 0^-a isn't defined
-        if (basis==0 && exponent <= 0) {
+        if (base==0 && exponent <= 0) {
             return Double.NaN;
         }
 
         double result = 1;
         //positiv exponents
         for (int i=0; i<exponent; i++) {
-            result *= basis;
+            result *= base;
         }
         //negativ exponents
         for (int i=0; i>exponent; i--) {
-            result /= basis;
+            result /= base;
         }
 
+        return result;
+    }
+
+    public double squareMultiply(final double base, final int exponent, final int modulus) {
+        if (modulus <= 0) {
+            throw new IllegalArgumentException("Only supports positive modulus.");
+        }
+        if (exponent < 0) {
+            return 1/squareMultiply(base, -exponent);
+        }
+        double result = 1;
+        final String binaryExponent= Integer.toBinaryString(exponent);
+        for (char c : binaryExponent.toCharArray()) {
+            result *= result;
+            result %= modulus;
+            if (c == '1') {
+                result *= base;
+                result %= modulus;
+            }
+        }
+        return result;
+    }
+
+    public double squareMultiply(final double base, final int exponent) {
+        if (exponent < 0) {
+            return 1/squareMultiply(base, -exponent);
+        }
+        double result = 1;
+        final String binaryExponent= Integer.toBinaryString(exponent);
+        for (char c : binaryExponent.toCharArray()) {
+            result *= result;
+            if (c == '1') {
+                result *= base;
+            }
+        }
         return result;
     }
 
